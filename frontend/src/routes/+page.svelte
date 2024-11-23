cca350b8a448ae9867dc6f916fdca3d2f9fee6f6
TesDevelopment <joshuaepee1@gmail.com>
2024-11-22 23:47:58 -0500

    Frontend changes
    Co-authored-by: Gus Ruben <gusruben@users.noreply.github.com>
    Co-authored-by: Mohamad Mortada <mohamad@mortada.dev>

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,13 +6,20 @@
 	const timeLeft = '12:31';
 </script>
 
-<div class="fixed inset-0 flex flex-col items-center gap-10 bg-counterspell-500 p-10">
-	<h1 class="font-retro text-3xl text-white">{connectedLocation}</h1>
-	<h1 class="absolute right-14 top-10 font-retro text-3xl text-white opacity-75">{timeLeft}</h1>
+<div class="fixed inset-0 flex flex-col items-center gap-10 bg-counterspell-500 px-36 py-16 ">
+	<div class="flex flex-row absolute left-14 top-10 justify-center items-center">
+		<img src="/favicon.png" alt="Counterspell Portal Logo" class="w-14 h-14" />
+		<h1 class=" font-retro text-3xl text-white">Counterspell Portal</h1>
+	</div>
+	<h1 class="font-retro text-3xl text-white mt-10">{connectedLocation}</h1>
+	<h1 class="absolute right-14 top-12 font-retro text-3xl text-white opacity-75">{timeLeft}</h1>
 	<div
-		class="relative h-full w-full border-4 border-dashed border-counterspell-pink bg-counterspell-100"
+		class="relative h-full w-full border-4 border-dashed border-counterspell-pink bg-counterspell-100 z-40"
 	>
 		<Portal bind:connectedLocation />
+	</div>
+	<div class="absolute bottom-0 z-10 w-full h-2/5">
+		<img src="/city.png" alt="City Background" class="w-full h-full object-cover" />
 	</div>
 </div>
 
