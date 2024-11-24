23f895d4915c094a715dca133d4078a5ee180040
Gus Ruben <gusruben@protonmail.com>
2024-11-24 11:30:09 -0500

    Start gallery page

---
+++ frontend/src/routes/gallery/+page.svelte
@@ -0,0 +1,11 @@
+<div class="fixed inset-0 flex flex-col items-center gap-10  px-36 py-16 ">
+	<div class="flex flex-row absolute left-14 top-10 justify-center items-center">
+		<img src="/favicon.png" alt="Counterspell Portal Logo" class="w-14 h-14" />
+		<h1 class=" font-retro text-3xl text-white">Gallery</h1>
+	</div>
+
+</div>
+
+<svelte:head>
+    <title>Gallery - Counterspell Portal</title>
+</svelte:head>
