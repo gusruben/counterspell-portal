a86dad155c75a9a9b2bfc5153eab7a2b52bea936
Gus Ruben <gusruben@protonmail.com>
2024-11-25 11:29:36 -0500

    Move styling to layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,24 @@
-<script>
+<script lang="ts">
+	import { Toaster } from 'svelte-sonner';
 	import '../app.css';
 </script>
 
+<Toaster toastOptions={{
+	unstyled: true,
+	classes: {
+		toast: "bg-[#0A081E] rounded-none border-counterspell-pink border-4 border-dashed text-white text-xl font-retro px-3"
+	}
+}}/>
+
 <slot></slot>
+
+<p class="absolute bottom-5 left-1/2 -translate-x-1/2 font-retro text-white opacity-20 hover:opacity-50 text-sm transition-all">
+	Made with ❤️ by <a class="underline" href="https://github.com/gusruben">Gus</a>, <a class="underline" href="https://github.com/TesDevelopment">Josh</a>, & <a class="underline" href="https://github.com/thedev132">Mohamad</a>
+</p>
+<img src="/city.png" alt="City Background" class="absolute w-full h-2/5 bottom-0 left-0 -z-50 object-cover" style="image-rendering: pixelated"/>
+
+<style>
+:global(body) {
+	background-color: #202f53;
+}
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -35,7 +35,7 @@
 	}
 }}/>
 
-<div class="fixed inset-0 flex flex-col items-center gap-10 bg-counterspell-500 px-36 py-16 ">
+<div class="fixed inset-0 flex flex-col items-center gap-10  px-36 py-16 ">
 	<div class="flex flex-row absolute left-14 top-10 justify-center items-center">
 		<img src="/favicon.png" alt="Counterspell Portal Logo" class="w-14 h-14" />
 		<h1 class=" font-retro text-3xl text-white">Counterspell Portal</h1>
@@ -47,15 +47,8 @@
 	>
 		<Portal bind:connectedLocation bind:timer/>
 	</div>
-	<div class="absolute bottom-0 z-10 w-full h-2/5">
-		<img src="/city.png" alt="City Background" class="w-full h-full object-cover" />
-	</div>
 </div>
 
-<p class="absolute bottom-5 left-1/2 -translate-x-1/2 font-retro text-white opacity-20 hover:opacity-50 text-sm transition-all">
-	Made with ❤️ by <a class="underline" href="https://github.com/gusruben">Gus</a>, <a class="underline" href="https://github.com/TesDevelopment">Josh</a>, & <a class="underline" href="https://github.com/thedev132">Mohamad</a>
-</p>
-
 <svelte:head>
-	<title>Counterspell Portal - {connectedLocation}</title>
+	<title>{connectedLocation} - Counterspell Portal</title>
 </svelte:head>
