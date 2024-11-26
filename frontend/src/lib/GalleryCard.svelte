c1be5a00ce1621df028381f4dbcc3686537cdf0e
Gus Ruben <gusruben@protonmail.com>
2024-11-26 11:19:42 -0500

    Fix play button positioning on gallery

--- frontend/src/lib/GalleryCard.svelte
+++ frontend/src/lib/GalleryCard.svelte
@@ -6,7 +6,7 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div on:click class="p-5 bg-[url(/dithering.png)] bg-cover hover:brightness-90 cursor-pointer" style="image-rendering: pixelated">
-    <div class="bg-black bg-opacity-20 flex flex-col w-64">
+    <div class="bg-black bg-opacity-20 flex flex-col w-64 relative">
         {#if URL.endsWith("mp4")}
             <!-- svelte-ignore a11y-media-has-caption -->
             <video src={URL} class="w-full h-40"></video>
